15e50aba102481a321e0599070965b0abb74d88d
Karina Zhubanazarova <nikarina9907.77@gmail.com>
2023-10-29 15:33:55 +0500

    remake laba2-14

--- laba2-14.sql
+++ laba2-14.sql
@@ -1,5 +1,6 @@
 /* Выберите самый дорогой и самый дешевый объект.*/
+
 USE cd;
-SELECT max(monthlymaintenance), 'дорогой' FROM facilities;
-SELECT facility,'дешевый' FROM facilities WHERE monthlymaintenance = (SELECT max(monthlymaintenance) FROM facilities)
-OR monthlymaintenance = (SELECT min(monthlymaintenance) FROM facilities);
+SELECT facility, 'Самый дорогой' AS 'Качественная стоимость' FROM facilities WHERE initialoutlay = (SELECT MAX(initialoutlay) FROM facilities)
+UNION 
+SELECT facility, 'Самый дешёвый' FROM facilities WHERE initialoutlay = (SELECT MIN(initialoutlay) FROM facilities);
