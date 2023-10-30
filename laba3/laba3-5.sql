443bbb479c30db4cf826d9185748d053adff7272
Karina Zhubanazarova <nikarina9907.77@gmail.com>
2023-10-31 01:24:44 +0500

    task/ 5

--- laba3/laba3-5.sql
+++ laba3/laba3-5.sql
@@ -1,5 +1,5 @@
 /*Выберите ФИО (== имя + фамилия) всех, кто покупал корты 1 и 2.*/
 USE cd;
-SELECT members.memid, facilities.facid, members.surname, members.firstname, facilities.facility
+SELECT members.surname, members.firstname, facilities.facility
 FROM members
 JOIN facilities ON (facilities.facility = 'Tennis Court 1' OR facilities.facility = 'Tennis Court 2' ) AND members.memid = facilities.facid;
