9f29b78da37f5bce134e8e7c633e05adfa976778
Karina Zhubanazarova <nikarina9907.77@gmail.com>
2023-11-13 11:42:55 +0500

    NEW laba3 task 2

--- laba3/laba3-2.sql
+++ laba3/laba3-2.sql
@@ -1,7 +1,7 @@
 /*Выберите теннисные корты, забронированные пользователями на 
 19 сентября 2012 года.*/
 USE cd;
-SELECT facilities.facid, facilities.facility, bookings.facid, bookings.starttime FROM facilities 
-JOIN bookings
-ON EXTRACT(DAY FROM starttime) = 19 AND EXTRACT(MONTH FROM starttime) = 9 
-AND EXTRACT(YEAR FROM starttime) = 2012 AND facility LIKE '%Tennis Court%';
+SELECT facilities.facility, bookings.starttime  
+  FROM facilities 
+    JOIN bookings ON bookings.starttime LIKE '%2012-09-19%' 
+  WHERE facilities.facility LIKE '%Tennis Court%';
