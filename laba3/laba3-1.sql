04e7578ef6475197b446913566dcda0c79f966a8
Karina Zhubanazarova <nikarina9907.77@gmail.com>
2023-11-13 11:34:23 +0500

    NEW laba3 task 1

--- laba3/laba3-1.sql
+++ laba3/laba3-1.sql
@@ -1,6 +1,8 @@
 /*Выберите лист времке  (timestamp) покупки времени 
 использования объектов членом клуба 'David Farrell'.*/
+
 USE cd;
-SELECT members.memid, bookings.memid, members.surname, members.firstname, bookings.starttime 
-FROM members
-JOIN bookings ON (surname = 'FARRELL' AND firstname = 'DAVID') AND members.memid = bookings.memid;
+SELECT bookings.starttime 
+  FROM members
+    JOIN bookings ON members.memid = bookings.memid
+  WHERE (members.surname = 'FARRELL' AND members.firstname = 'DAVID') ;
