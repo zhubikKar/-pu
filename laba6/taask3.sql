36abc34f1df8fdf72b398a313cff1c8e61b5f52d
Karina Zhubanazarova <nikarina9907.77@gmail.com>
2023-11-27 13:29:02 +0500

    lb6 task4

---
+++ laba6/taask3.sql
@@ -0,0 +1,5 @@
+/*Выберите процент использования объектов по месяцам, упорядочив по возрастанию*/
+USE cd;
+SELECT monthname(starttime) as месяц, round(count(slots)/(SELECT count(*) FROM bookings)*100, 2) AS 'Использование объекта, %' 
+FROM bookings
+GROUP BY месяц ORDER BY 2;
