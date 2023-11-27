5c0325a513586c5310f27576c64a5b92298f64f6
Karina Zhubanazarova <nikarina9907.77@gmail.com>
2023-11-27 11:12:00 +0500

    NEW

--- laba3/laba3-5.sql
+++ laba3/laba3-5.sql
@@ -3,4 +3,4 @@ USE cd;
 SELECT DISTINCT members.surname, members.firstname, facilities.facility
   FROM members
     JOIN facilities ON members.memid = facilities.facid
-  WHERE  (facilities.facility = 'Tennis Court 1' OR facilities.facility = 'Tennis Court 2');
+  WHERE  (facilities.facility LIKE '%Court 1' OR facilities.facility LIKE '%Court 2');
--- laba4/task3.sql
+++ laba4/task3.sql
@@ -1,5 +1,6 @@
 /* добавляем еще одно спа с такими же характеристиками как в задании 1, сгенерировав новое айди */
 USE cd;
 INSERT facilities
-SELECT (SELECT MAX(facid)+1 FROM facilities), facility, membercost, guestcost, initialoutlay, monthlymaintenance FROM facilities where facid = 9;
+SELECT (SELECT MAX(facid)+1 FROM facilities), facility, membercost, guestcost, initialoutlay, monthlymaintenance FROM facilities 
+WHERE facility = 'spa' LIMIT 1;
 SELECT * FROM facilities;
