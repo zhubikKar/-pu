027f214f0f5a30ddc78e2a562be0842c7d8c6ec0
Karina Zhubanazarova <nikarina9907.77@gmail.com>
2023-11-27 13:19:42 +0500

    TASK4

--- laba5/task4.sql
+++ laba5/task4.sql
@@ -1,6 +1,6 @@
 /*Рассчитайте количество аренд каждого из объектов клуба.*/
 USE cd;
-SELECT facility AS 'Объекты клуба', SUM(book.slots) AS 'Кол-во аренд'
+SELECT facility AS 'Объекты клуба', SUM(b.slots) AS 'Кол-во аренд'
 FROM facilities AS f
 INNER JOIN bookings AS b ON b.facid = f.facid
 GROUP BY f.facid;
