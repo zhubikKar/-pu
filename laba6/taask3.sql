95fc8c6f7367c3b020ffc2bb92d806a0fe9383eb
Karina Zhubanazarova <nikarina9907.77@gmail.com>
2023-12-06 01:55:14 +0500

    new task3

--- laba6/taask3.sql
+++ laba6/taask3.sql
@@ -1,5 +1,14 @@
 /*Выберите процент использования объектов по месяцам, упорядочив по возрастанию*/
 USE cd;
-SELECT monthname(starttime) as месяц, round(count(slots)/(SELECT count(*) FROM bookings)*100, 2) AS 'Использование объекта, %' 
-FROM bookings
-GROUP BY месяц;
+WITH slots as (SELECT facility, SUM(b.slots) AS CostRent,   
+DATE_FORMAT(b.starttime, '%y %m') AS YearMonth
+FROM facilities AS f
+JOIN bookings AS b ON b.facid = f.facid
+GROUP BY f.facid, YearMonth)
+SELECT s1.facility AS 'Объекты', 
+ROUND(s1.CostRent / SUM(s2.CostRent) * 100, 1) AS 'Использование объекта, %',
+s1.YearMonth
+FROM slots AS s1
+JOIN slots AS s2 ON s1.YearMonth = s2.YearMonth
+GROUP BY s1.facility, s1.CostRent, s1.YearMonth
+ORDER BY 'Использование объекта, %' ASC;
