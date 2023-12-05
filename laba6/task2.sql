1e1b5676d49b27a240681a84e7a94709753623f7
Karina Zhubanazarova <nikarina9907.77@gmail.com>
2023-12-06 01:44:35 +0500

    new task2

--- laba6/task2.sql
+++ laba6/task2.sql
@@ -1,5 +1,6 @@
 /*Выберите количество аренд объектов клуба в каждый месяц.*/
 USE cd;
-SELECT monthname(starttime) AS месяц, count(slots) AS слоты 
-FROM bookings
-GROUP BY месяц;
+SELECT YEAR(starttime) AS Год, MONTH(starttime) AS Месяц, facility AS 'Название объекта', SUM(b.slots) AS 'Количество аренд'
+FROM bookings AS b
+JOIN facilities AS f ON b.facid = f.facid
+GROUP BY YEAR(starttime), MONTH(starttime), facility;
