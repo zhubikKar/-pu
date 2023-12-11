3e7311876e4c7f784bfb9747c4dccf770312afa5
Karina Zhubanazarova <nikarina9907.77@gmail.com>
2023-12-11 11:34:31 +0500

    task5

--- laba5/task5.sql
+++ laba5/task5.sql
@@ -1,6 +1,6 @@
 /*Рассчитайте количество аренд каждого из объектов клуба за сентябрь 2012 года.*/
 USE cd;
-SELECT facility AS 'Объекты клуба', SUM(book.slots) AS 'Кол-во аренд'
+SELECT facility AS 'Объекты клуба', SUM(b.slots) AS 'Кол-во аренд'
 FROM facilities AS f
 INNER JOIN bookings AS b ON b.facid = f.facid
 WHERE b.starttime >= '2012-09-01' AND b.starttime < '2012-10-01'
