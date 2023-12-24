cb6557ac20a911728efa4f8c1a6e6f738eba0b18
Karina Zhubanazarova <nikarina9907.77@gmail.com>
2023-12-24 14:41:57 +0500

    task3

---
+++ laba7/task3.sql
@@ -0,0 +1,18 @@
+/*Напишите процедуру, которая считает окупаемость каждого объекта
+ клуба на основании оплаты аренд за месяц. Применить ее к июлю 2012 года. */
+USE cd;
+DELIMITER //
+DROP PROCEDURE IF EXISTS paymentin //
+CREATE PROCEDURE paymentin(yourdate DATE)
+  BEGIN
+    SELECT b.facid, f.facility, SUM(p.payment) - f.monthlymaintenance AS occupate
+	FROM bookings AS b
+	INNER JOIN payments AS p ON b.bookid = p.bookid
+	INNER JOIN facilities AS f ON b.facid = f.facid
+	WHERE DATE_FORMAT(starttime, '%y %m') = DATE_FORMAT(yourdate, '%y %m')
+	GROUP BY b.facid 
+    ORDER BY b.facid;
+  END //
+DELIMITER ;
+
+CALL paymentin('2012-07-01');
