a967fd13e538d9f494979507ed4c19237b6fdb24
Karina Zhubanazarova <nikarina9907.77@gmail.com>
2023-12-24 14:18:17 +0500

    task1

--- laba7/task1.sql
+++ laba7/task1.sql
@@ -1,7 +1,9 @@
 /*Создайте функцию, которая рассчитывает стоимость каждой аренды (для каждой записи таблицы bookings)*/
 USE cd;
-DELIMITER $$
-DROP FUNCTION IF EXISTS Calculater $$
+
+DELIMITER //
+
+DROP FUNCTION IF EXISTS Calculater //
 CREATE FUNCTION Calculater(memid INT, facid INT, slots INT)
 RETURNS DECIMAL(10, 2)
 DETERMINISTIC
@@ -10,7 +12,9 @@ BEGIN
 	SET cost = (SELECT IF(memid = 0, guestcost * slots, membercost * slots)
 	FROM facilities AS f WHERE facid = f.facid);
 	RETURN cost;
-END$$
+END //
+
 DELIMITER ;
+
 SELECT Calculater(memid, facid, slots) AS cost
 FROM bookings;
